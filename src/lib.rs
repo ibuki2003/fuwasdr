c4efe70b7d8a81d82df79c8ad9dc8cf24a6b7e4c
ibuki2003 <main@fuwa.dev>
2024-03-30 19:56:37 +0900

    chore: panic halt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@
 pub const SAMPLE_RATE: usize = 192_000;
 
 use defmt_rtt as _;
+#[cfg(debug_assertions)]
 use panic_probe as _;
+
+#[cfg(not(debug_assertions))]
+extern crate panic_halt;
 
 pub use rp2040_hal as hal;
 pub use rp_pico as bsp;
